b904fa3ce6899854e844ccae4f3218e85ec9c2ae
asam® <asamshah@gmail.com>
2024-09-23 21:08:58 +0100

    Added Scroll to Top functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,19 @@ import './Header.css';
 import finsplash from '../finsplash.svg';
 
 const Header = () => {
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   return (
     <header className="header">
       <div className="header-container">
-      <img src={finsplash} alt="Finsplash" style={{ width: '100px', height: '100px', objectFit: 'contain' }} />
+        <img
+          src={finsplash}
+          alt="Finsplash"
+          style={{ width: '110px', height: '110px', objectFit: 'contain', cursor: 'pointer' }}
+          onClick={scrollToTop}
+        />
       </div>
     </header>
   );
